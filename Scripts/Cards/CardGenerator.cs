74ac0b8f9c471c8c6a2ff193f2e03158b559d879
TanGlennda <t.glennda@gmail.com>
2017-02-17 16:38:02 +0800

    Glennda: Main Menu Audio + Pause
    Amirul: Card Class (Final), Card Generator, Card Resources, Card Storage

---
+++ Scripts/Cards/CardGenerator.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CardGenerator : MonoBehaviour {
+
+    string[] NameOfCards = {"BronzeArmour",
+                           "BronzeSword",
+                           "SilverArmour",
+                           "SilverSword",
+                           "GoldArmour",
+                           "GoldSword",
+                           "GeneralsVisage",
+                           "GeneralsBlade",
+                           "ChallengersMantle",
+                           "ChallengersDestructor"};
+
+    public List<GameObject> Deck = new List<GameObject>();
+    GameObject Card;
+    int i;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    //WHEN PLAYER KILLS ENEMY
+    void GenerateCard()
+    {
+        i = (Random.Range(0, NameOfCards.Length));
+        Card = Instantiate(Resources.Load(NameOfCards[i]), new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+        Card.GetComponent<CardData>();
+        Card.transform.parent = this.transform;
+    }
+
+    void PickUpCard(GameObject DroppedCard)
+    {
+        Deck.Add(DroppedCard);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetKeyDown("space"))
+        {
+            GenerateCard();
+        }
+        if(Input.GetMouseButtonDown(0))
+        {
+            PickUpCard(Card);
+        }
+	}
+}
